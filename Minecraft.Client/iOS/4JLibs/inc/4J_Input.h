0a5bc23e9896efc7656ee003ad5d7991be26755a
bilawal <264871033+dtentiion@users.noreply.github.com>
2026-04-29 03:34:49 +0100

    stubs: SetBanListCheck/IsLocalMultiplayerAvailable/SessionHasSpace/ButtonPressed/IsPadConnected + convStringToWstring(wchar_t*) overload

--- Minecraft.Client/iOS/4JLibs/inc/4J_Input.h
+++ Minecraft.Client/iOS/4JLibs/inc/4J_Input.h
@@ -16,6 +16,9 @@ class C_4JInput {
 public:
     template<class... A> int   GetIdleSeconds(A...)         { return 0; }
     template<class... A> bool  IsButtonPressed(A...)        { return false; }
+    // Older upstream code uses `ButtonPressed` and `IsPadConnected`.
+    template<class... A> bool  ButtonPressed(A...)           { return false; }
+    template<class... A> bool  IsPadConnected(A...)          { return true; }
     template<class... A> bool  IsButtonReleased(A...)       { return false; }
     template<class... A> bool  IsButtonHeld(A...)           { return false; }
     template<class... A> float GetAxisValue(A...)           { return 0.0f; }
